3f852cc7dce44a531e3a7550e4b9c24ef25b6d77
Mountain Inn <blckclst@gmail.com>
2024-09-13 02:01:36 +0500

    Ressurect Energy.cs
    This reverts commit 950d23a6aad8018c6f421ef0bf7d01e2af9e4a31.

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Zenject;
+
+[CreateAssetMenu(fileName = "Energy", menuName = "SO/Talents/Energy")]
+public class Energy : Stat, ITickable
+{
+    [SerializeField] [HideInInspector] List<Field> maximumEnergy;
+    [SerializeField] [HideInInspector] List<Field> regenPerSecond;
+
+    [Inject] Character character;
+    [Inject]
+    public void Construct()
+    {
+        character.AddTickable(this);
+    }
+
+    public override int CurrentValue => maximumEnergy[Level];
+
+    public override IObservable<string> ObserveDescription()
+    {
+        return
+            this.buyableLevel.ware.level.current
+            .Select(l =>
+                    GetFieldDescriptions((int)l,
+                                         ("Maximum Energy", maximumEnergy),
+                                         ("Regen", regenPerSecond)
+                    ));
+    }
+
+    protected override void OnLevelUp(int level, Price price)
+    {
+        CostUp(level, price);
+        character.energy.ResizeAndRefill(CurrentValue);
+    }
+
+    public void Tick()
+    {
+        if (!character.energy.IsFull)
+        {
+            float amount =
+                regenPerSecond[Level]
+                * Time.deltaTime;
+
+            character.energy.Add(amount);
+        }
+    }
+}
